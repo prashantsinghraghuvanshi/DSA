15ad2cc1cd6ecfa48ee88fec5f72f2f27f32c1ce
Prashant Singh <106803837+prashantsinghraghuvanshi@users.noreply.github.com>
2023-11-07 15:53:49 +0000

    Done : 235.Lowest Common Ancestor of a BT

---
+++ leetCode/235-LowestCommonAncestorofaBinarySearchTree.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q){
+        //base case
+        if(root==NULL || root==p || root==q)    return root;      
+        //traversal
+        TreeNode *left=lowestCommonAncestor(root->left,p,q);
+        TreeNode *right=lowestCommonAncestor(root->right,p,q);
+        //return
+        if(left==NULL) return right;
+        else if(right==NULL) return left;
+        //answer is found
+        else{       
+            return root;
+        }
+    }
+};
